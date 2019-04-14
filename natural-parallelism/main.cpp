2c3607441b39ab5a90e14c7b0b865d511e6ce1ee
Andrei Klitsunou <baklagan96@gmail.com>
2019-04-15 00:47:47 +0300

    Optimize multiplications

--- natural-parallelism/main.cpp
+++ natural-parallelism/main.cpp
@@ -13,16 +13,19 @@ using tensor2d = std::vector< std::vector<double> >;
 using tensor1d = std::vector<double>;
 
 void start_initialization(tensor3d& y, double h, int N) {
+    double x_curr, y_curr;
     for (int i = 0 ; i <= N ; i++) {
+        x_curr = i * h;
         for (int j = 0; j <= N; j++) {
+            y_curr = j * h;
             for (int k = 0; k <= N; k++) {
-                y[i][j][k] = func::u0({i*h, j*h, k*h});
+                y[i][j][k] = func::u0({x_curr, y_curr, k*h});
             }
         }
     }
 }
 
-int main(int argc, char * argv[]) {
+int main(int argc, char* argv[]) {
     auto start = std::chrono::steady_clock::now();
     int N = 100; // number of iterations (start conditions intializing not included)
     if (argc == 2) {
@@ -41,16 +44,20 @@ int main(int argc, char * argv[]) {
     start_initialization(y, h, N);
 
     double eps = 2 * h * h / t;
-
+    double x_curr, y_curr, t_curr;
     for (int j = 0; j < j0; j++) {
+        t_curr = t * j;
+        double row_curr, col_curr;
         for (int i = 0; i <= N ; i++) {
+            row_curr = i * h;
             for (int k = 0; k <= N ; k++) {
-                y[0][i][k] = func::a0({i*h, k*h}, (j)*t);
-                y[N][i][k] = func::a1({i*h, k*h}, (j)*t);
-                y[i][0][k] = func::b0({i*h, k*h}, (j)*t);
-                y[i][N][k] = func::b1({i*h, k*h}, (j)*t);
-                y[i][k][0] = func::c0({i*h, k*h}, (j)*t);
-                y[i][k][N] = func::c1({i*h, k*h}, (j)*t);
+                col_curr = k * h;
+                y[0][i][k] = func::a0({row_curr, col_curr}, t_curr);
+                y[N][i][k] = func::a1({row_curr, col_curr}, t_curr);
+                y[i][0][k] = func::b0({row_curr, col_curr}, t_curr);
+                y[i][N][k] = func::b1({row_curr, col_curr}, t_curr);
+                y[i][k][0] = func::c0({row_curr, col_curr}, t_curr);
+                y[i][k][N] = func::c1({row_curr, col_curr}, t_curr);
             }
         }
 
@@ -114,10 +121,13 @@ int main(int argc, char * argv[]) {
 
         double error = 0;
         for (int i1 = 0; i1 <= N; ++i1) {
+            double t_curr = (j + 1) * t;
+            double x_curr = i1 * h;
             for (int i2 = 0; i2 <= N; ++i2) {
+                double y_curr = i2 * h;
                 for (int i3 = 0; i3 <= N; ++i3) {
-                    if (std::abs(func::u({i1*h, i2*h, i3*h}, (j+1)*t) - y[i1][i2][i3]) > error) {
-                        error = std::abs(func::u({i1*h, i2*h, i3*h}, (j+1)*t) - y[i1][i2][i3]);
+                    if (std::abs(func::u({x_curr, y_curr, i3*h}, t_curr) - y[i1][i2][i3]) > error) {
+                        error = std::abs(func::u({x_curr, y_curr, i3*h}, t_curr) - y[i1][i2][i3]);
                     }
                 }
             }
