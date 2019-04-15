24b520ccef246b85eb3751ca4ad84e9eb197e887
Andrei Klitsunou <baklagan96@gmail.com>
2019-04-15 11:57:38 +0300

    Another executable rename

--- improved-locality/compile.sh
+++ improved-locality/compile.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 REPO_ROOT="$SCRIPT_DIR/../"
 
 cd $REPO_ROOT
-mpic++ -std=c++14 -I $REPO_ROOT -O2 improved-locality/mpi_version.cc common/test_functions.cc -o improved_mpi_version.out
+mpic++ -std=c++14 -I $REPO_ROOT -O2 improved-locality/mpi_version.cc common/test_functions.cc -o mpi_version_imporved.out
