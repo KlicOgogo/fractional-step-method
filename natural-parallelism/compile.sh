cb46fa9dd63d365af9b28443215ba5a6bee5020a
Andrei Klitsunou <baklagan96@gmail.com>
2019-04-15 10:11:54 +0300

    Different .out filenames for mpi versions

--- natural-parallelism/compile.sh
+++ natural-parallelism/compile.sh
@@ -7,4 +7,4 @@ REPO_ROOT="$SCRIPT_DIR/../"
 
 cd $REPO_ROOT
 mpic++ -std=c++14 -I $REPO_ROOT -O2 natural-parallelism/sequential.cc common/test_functions.cc -o sequential.out
-mpic++ -std=c++14 -I $REPO_ROOT -O2 natural-parallelism/mpi_version.cc common/test_functions.cc -o mpi_version.out
+mpic++ -std=c++14 -I $REPO_ROOT -O2 natural-parallelism/mpi_version.cc common/test_functions.cc -o natural_mpi_version.out
