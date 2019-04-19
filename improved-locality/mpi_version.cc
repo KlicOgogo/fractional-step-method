0dd3d6948ddbd2f23d6f4e04f8100426ccf85dc6
Andrei Klitsunou <baklagan96@gmail.com>
2019-04-19 22:19:19 +0300

    Same Ns for both versions

--- improved-locality/mpi_version.cc
+++ improved-locality/mpi_version.cc
@@ -1,6 +1,6 @@
 /* TODO: change MPI_Send and MPI_Recv to non blocking calls; reduce memory usage
  *
- * ACHTUNG: assume N % pcnt =  0
+ * ACHTUNG: assume N+1 % pcnt =  0
  */
 
 #include <cmath>
@@ -12,24 +12,24 @@
 #include "common/structures.h"
 #include "common/test_functions.h"
 
-constexpr int N = 100;
-const double h = consts::l / N; // grid step
+constexpr int N = 99;
+const double h = consts::l / (N+1); // grid step
 constexpr int Q2 = 30;
 constexpr int Q3 = 30;
 
-double y[consts::j0][N][N][N] = {{{{0.}}}};
-double a0[N][N][consts::j0];
-double a1[N][N][consts::j0];
-double b0[N][N][consts::j0];
-double b1[N][N][consts::j0];
-double c0[N][N][consts::j0];
-double c1[N][N][consts::j0];
+double y[consts::j0][N+1][N+1][N+1] = {{{{0.}}}};
+double a0[N+1][N+1][consts::j0];
+double a1[N+1][N+1][consts::j0];
+double b0[N+1][N+1][consts::j0];
+double b1[N+1][N+1][consts::j0];
+double c0[N+1][N+1][consts::j0];
+double c1[N+1][N+1][consts::j0];
 
 int i, i1, i2, i3, j;
 
 void setBorderConditions() {
-	for (i1 = 0; i1 < N; ++i1) {
-		for (i2 = 0; i2 < N; ++i2) {
+	for (i1 = 0; i1 < N+1; ++i1) {
+		for (i2 = 0; i2 < N+1; ++i2) {
 			for (j = 0; j < consts::j0; ++j) {
 				a0[i1][i2][j] = func::a0({i1 * h, i2 * h}, (j + 1.0 / 3.0) * consts::t);
 				a1[i1][i2][j] = func::a1({i1 * h, i2 * h}, (j + 1.0 / 3.0) * consts::t);
@@ -43,18 +43,18 @@ void setBorderConditions() {
 }
 
 void setInitialApproximation() {
-	for (i1 = 0; i1 < N; ++i1) {
-		for (i2 = 0; i2 < N; ++i2) {
-			for (i3 = 0; i3 < N; ++i3) {
+	for (i1 = 0; i1 < N+1; ++i1) {
+		for (i2 = 0; i2 < N+1; ++i2) {
+			for (i3 = 0; i3 < N+1; ++i3) {
 				y[0][i1][i2][i3] = func::u0({i1 * h, i2 * h, i3 * h});
 			}
 		}
 	}
 }
 
-double tempY[2][N][N][N];
-double alphaArr[N][N][N];
-double betaArr[N][N][N];
+double tempY[2][N+1][N+1][N+1];
+double alphaArr[N+1][N+1][N+1];
+double betaArr[N+1][N+1][N+1];
 
 int main(int argc, char* argv[]) {
 	std::ofstream output_file;
@@ -67,9 +67,9 @@ int main(int argc, char* argv[]) {
 
 	auto start = std::chrono::steady_clock::now();
 
-	const int r1 = N / pcnt; // should be N % pcnt = 0
-	const int r2 = (N + Q2 - 1) / Q2;
-	const int r3 = (N + Q3 - 1) / Q3;
+	const int r1 = (N+1) / pcnt; // should be N(+1) % pcnt = 0
+	const int r2 = (N+1 + Q2 - 1) / Q2;
+	const int r3 = (N+1 + Q3 - 1) / Q3;
 
 	setBorderConditions();
 	setInitialApproximation();
@@ -77,8 +77,8 @@ int main(int argc, char* argv[]) {
 	double eps = 2.0 * h * h / consts::t;
 
 	constexpr int double_size = sizeof(eps);
-	tensor1d alpha = tensor1d(N);
-	tensor1d beta = tensor1d(N);
+	tensor1d alpha = tensor1d(N+1);
+	tensor1d beta = tensor1d(N+1);
 	const size_t splitSize = r2 * r3;
 	tensor1d alphaLast = tensor1d(splitSize);
 	tensor1d betaLast = tensor1d(splitSize);
@@ -93,15 +93,15 @@ int main(int argc, char* argv[]) {
 					MPI_Recv(betaLast.data(), splitSize, MPI_DOUBLE, my_rank - 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 				}
 
-				for (int i2 = r2 * q2; i2 < std::min(r2 * (q2 + 1), N); ++i2) {
-					for (int i3 = r3 * q3; i3 < std::min(r3 * (q3 + 1), N); ++i3) {
+				for (int i2 = r2 * q2; i2 < std::min(r2 * (q2 + 1), N+1); ++i2) {
+					for (int i3 = r3 * q3; i3 < std::min(r3 * (q3 + 1), N+1); ++i3) {
 						int lastIdx = (i2 - r2 * q2) * r2 + i3 - r3 * q3;
 						if (my_rank == 0) {
 							alphaLast[lastIdx] = 0.0;
 							betaLast[lastIdx] = a0[i2][i3][j];
 						}
 						const int startIdx = my_rank * r1 + 1;
-						const int stopIdx = std::min((my_rank + 1) * r1 + 1, N - 1);
+						const int stopIdx = std::min((my_rank + 1) * r1 + 1, N+1 - 1);
 						alphaArr[startIdx - 1][i2][i3] = alphaLast[lastIdx];
 						betaArr[startIdx - 1][i2][i3] = betaLast[lastIdx];
 						for (i = startIdx; i < stopIdx; ++i) {
@@ -125,13 +125,13 @@ int main(int argc, char* argv[]) {
 					// TODO: change to non blocking calls
 					MPI_Recv(yLast.data(), splitSize, MPI_DOUBLE, my_rank + 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 				}
-				for (i2 = r2 * q2; i2 < std::min(r2 * (q2 + 1), N); ++i2) {
-					for (i3 = r3 * q3; i3 < std::min(r3 * (q3 + 1), N); ++i3) {
+				for (i2 = r2 * q2; i2 < std::min(r2 * (q2 + 1), N+1); ++i2) {
+					for (i3 = r3 * q3; i3 < std::min(r3 * (q3 + 1), N+1); ++i3) {
 						int lastIdx = (i2 - r2 * q2) * r2 + i3 - r3 * q3;
 						int startIdx = (my_rank + 1) * r1 - 1;
 						if (my_rank == pcnt - 1) {
 							yLast[lastIdx] = a1[i2][i3][j];
-							startIdx = N - 2;
+							startIdx = N+1 - 2;
 						}
 						const int stopIdx = my_rank * r1;
 						tempY[0][startIdx+1][i2][i3] = yLast[lastIdx];
@@ -149,30 +149,30 @@ int main(int argc, char* argv[]) {
 		}
 
 		for (i1 = r1 * my_rank; i1 < r1 * (my_rank + 1); ++i1) {
-			for (i3 = 0; i3 < N; ++i3) {
+			for (i3 = 0; i3 < N+1; ++i3) {
 				alpha[0] = 0;
 				beta[0] = b0[i1][i3][j];
-				for (i = 1; i < N - 1; ++i) {
+				for (i = 1; i < N+1 - 1; ++i) {
 					alpha[i] = 1.0 / (2.0 + eps - alpha[i-1]);
 					beta[i] = (tempY[0][i1][i+1][i3] + tempY[0][i1][i-1][i3] + beta[i-1] + (eps - 2.0) * tempY[0][i1][i][i3]) * alpha[i];
 				}
-				tempY[1][i1][N - 1][i3] = b1[i1][i3][j];
-				for (i = N - 2; i >= 0; --i) {
+				tempY[1][i1][N+1 - 1][i3] = b1[i1][i3][j];
+				for (i = N+1 - 2; i >= 0; --i) {
 					tempY[1][i1][i][i3] = alpha[i] * tempY[1][i1][i+1][i3] + beta[i];
 				}
 			}
 		}
 
 		for (i1 = r1 * my_rank; i1 < r1 * (my_rank + 1); ++i1) {
-			for (i2 = 0; i2 < N; ++i2) {
+			for (i2 = 0; i2 < N+1; ++i2) {
 				alpha[0] = 0;
 				beta[0] = c0[i1][i2][j];
-				for (i = 1; i < N - 1; ++i) {
+				for (i = 1; i < N+1 - 1; ++i) {
 					alpha[i] = 1.0 / (2.0 + eps - alpha[i-1]);
 					beta[i] = (tempY[1][i1][i2][i+1] + tempY[1][i1][i2][i-1] + beta[i-1] + (eps - 2.0) * tempY[1][i1][i2][i]) * alpha[i];
 				}
-				y[j+1][i1][i2][N-1] = c1[i1][i2][j];
-				for (i = N - 2; i >= 0; --i) {
+				y[j+1][i1][i2][N+1-1] = c1[i1][i2][j];
+				for (i = N+1 - 2; i >= 0; --i) {
 					y[j+1][i1][i2][i] = alpha[i] * y[j + 1][i1][i2][i+1] + beta[i];
 				}
 			}
@@ -180,18 +180,18 @@ int main(int argc, char* argv[]) {
 
 		// TODO: change to non blocking calls
 		if (my_rank != pcnt - 1) {
-			MPI_Recv(y[j + 1][(my_rank + 1) * r1 + 1], N * N, MPI_DOUBLE, my_rank + 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			MPI_Recv(y[j + 1][(my_rank + 1) * r1], N * N, MPI_DOUBLE, my_rank + 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(y[j + 1][(my_rank + 1) * r1 + 1], (N+1) * (N+1), MPI_DOUBLE, my_rank + 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(y[j + 1][(my_rank + 1) * r1], (N+1) * (N+1), MPI_DOUBLE, my_rank + 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		}
 		if (my_rank) {
-			MPI_Send(y[j + 1][my_rank * r1 + 1], N * N, MPI_DOUBLE, my_rank - 1, 0, MPI_COMM_WORLD);
-			MPI_Send(y[j + 1][my_rank * r1], N * N, MPI_DOUBLE, my_rank - 1, 0, MPI_COMM_WORLD);
+			MPI_Send(y[j + 1][my_rank * r1 + 1], (N+1) * (N+1), MPI_DOUBLE, my_rank - 1, 0, MPI_COMM_WORLD);
+			MPI_Send(y[j + 1][my_rank * r1], (N+1) * (N+1), MPI_DOUBLE, my_rank - 1, 0, MPI_COMM_WORLD);
 		}
 
 		double error = 0.0;
 		for (i1 = my_rank * r1; i1 < (my_rank + 1) * r1; ++i1) {
-			for (i2 = 0; i2 < N; ++i2) {
-				for (i3 = 0; i3 < N; ++i3) {
+			for (i2 = 0; i2 < N+1; ++i2) {
+				for (i3 = 0; i3 < N+1; ++i3) {
 					error = std::max(error, std::abs(func::u({h * i1, h * i2, h * i3}, consts::t * j) - y[j][i1][i2][i3]));
 				}
 			}
